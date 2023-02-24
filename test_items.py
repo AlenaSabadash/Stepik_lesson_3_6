be3ecd2f2502e864e274afa52ebdc0c17454c290
Alena Sabadash <alena_sbsh@mail.ru>
2023-02-24 12:49:12 +0500

    Added test_is_add_button_enabled

--- test_items.py
+++ test_items.py
@@ -8,8 +8,7 @@ from selenium.webdriver.common.by import By
         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/",
     ],
 )
-def test_check_language(browser, url):
+def test_is_add_button_enabled(browser, url):
     browser.get(url)
-    add_button = browser.find_element(By.XPATH, '//button[@value="Ajouter au panier"]')
-    print("add_button.text: ", add_button.text)
-    assert add_button.text == "Ajouter au panier"
+    add_button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket").is_enabled()
+    assert add_button is True
